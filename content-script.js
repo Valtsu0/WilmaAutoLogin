32f644fd52c55d687b0c5c6ee2c487ab70fa579f
Valtsu0 <30974789+Valtsu0@users.noreply.github.com>
2021-10-28 12:43:44 +0300

    Update content-script.js
    Removed unused code
    Renamed a variable to be more descriptive

--- content-script.js
+++ content-script.js
@@ -2,25 +2,10 @@ function simulateClick(obj) {
   var evt = document.createEvent("MouseEvents");
   evt.initMouseEvent("click", true, true, window,
     0, 0, 0, 0, 0, false, false, false, false, 0, null);
-  console.log("Clicked")
-  console.log(obj)
-  var canceled = !obj.dispatchEvent(evt);      
-/*
-
-  if(canceled) {
-    // A handler called preventDefault
-    alert("canceled");
-  } else {
-    // None of the handlers called preventDefault
-    alert("not canceled");
-  }
-  */
+  obj.dispatchEvent(evt);
 }
 
-var what = document.getElementsByClassName("pull-left oid-login-button")
-if (what.length > 0) {
-	console.log("Login page detected. Executing click protocol")
-	simulateClick(what[0]);
-} else {
-	console.log("No login button detected.")
+var elements = document.getElementsByClassName("pull-left oid-login-button")
+if (elements.length > 0) {
+	simulateClick(elements[0]);
 }
